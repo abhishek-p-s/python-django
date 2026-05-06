cb55e412eada4027249a1b752f230fb3894b3fad
abhishek_p_s <abhishekps6282@gmail.com>
2026-05-06 11:54:56 +0530

    list and fetch by id api created for tutorial

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from watchlist_app import models
+from django.http import JsonResponse
 
-# Create your views here.
+def movie_list(request):
+    movies = models.Movie.objects.all()
+    data={
+        'movies':list(movies.values())
+    }
+    return JsonResponse(data) # will only work with dictionaries
+    
+    
+def movie_details(request, id):
+    movie = models.Movie.objects.get(id=id)
+    data={
+        'name':movie.name,
+        'description':movie.description,
+        'active':movie.active
+    }
+    return JsonResponse(data)    
